794ce3d417636d197fa29dcd421f559f7a8ff600
microsong <jhn06046@hotmail.com>
2019-07-18 10:14:35 +0900

    5. 게임 매니저 스크립트

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Singleton 변수
+    public static GameManager instance = null;
+
+    public BoardManager boardScript;
+    private int level = 4;  // Level이 3인 이유는 log2로 되어있기 때문
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        // Singleton 체크
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
         
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    
+    void InitGame()
+    {
+        boardScript.SetUpScene(level);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    // gameManager변수
+    public GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //게임매니저의 인스턴스가 null일 경우에 게임 매니저 프리팹을 인스턴스화
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
